7f8765f6b342d821b599af3ded58876af75a9a80
hugonewton <hugovlnv@gmail.com>
2023-09-05 13:55:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,39 @@ console.log("ntm");
 console.log("ntm");
 console.log("ntm");
 console.log("ntm");
+
+const images = document.getElementsByClassName("img-souvenir");
+
+let globalIndex = 0,
+    last = { x: 0, y: 0 };
+
+const activate = (image, x, y) => {
+  image.style.left = `${x}px`;
+  image.style.top = `${y}px`;
+  image.style.zIndex = globalIndex;
+
+  image.dataset.status = "active";
+
+  last = { x, y };
+}
+
+const distanceFromLast = (x, y) => {
+  return Math.hypot(x - last.x, y - last.y);
+}
+
+const handleOnMove = e => {
+  if(distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / 20)) {
+    const lead = images[globalIndex % images.length],
+          tail = images[(globalIndex - 5) % images.length];
+
+    activate(lead, e.clientX, e.clientY);
+
+    if(tail) tail.dataset.status = "inactive";
+    
+    globalIndex++;
+  }
+}
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
