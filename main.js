18d55d9d2705607ba4a7272894516124faa4718d
hugonewton <hugovlnv@gmail.com>
2023-09-06 17:23:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,45 +5,6 @@ console.log("ntm");
 console.log("ntm");
 
 
-// IMAGE TRAIL
-const images = document.getElementsByClassName("img-souvenir");
-let changeSpeed = 20;
-
-let globalIndex = 0,
-    last = { x: 0, y: 0 };
-
-const activate = (image, x, y) => {
-  image.style.left = `${x}px`;
-  image.style.top = `${y}px`;
-  image.style.zIndex = globalIndex;
-
-  image.dataset.status = "active";
-
-  last = { x, y };
-}
-
-const distanceFromLast = (x, y) => {
-  return Math.hypot(x - last.x, y - last.y);
-}
-
-const handleOnMove = e => {
-  if(distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / changeSpeed)) {
-    const lead = images[globalIndex % images.length],
-          tail = images[(globalIndex - 5) % images.length];
-
-    activate(lead, e.clientX, e.clientY);
-
-    if(tail) tail.dataset.status = "inactive";
-    
-    globalIndex++;
-  }
-}
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
-
-console.log("hello");
 
 
 
